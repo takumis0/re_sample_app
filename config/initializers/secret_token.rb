eea0f7ab7411eeaf1f4f9b4b595948cbdab82f84
Takumi Sekiguchi <takumis2013@yahoo.co.jp>
2017-12-02 23:49:58 +0000

    Initialize commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ReSampleApp::Application.config.secret_key_base = '03f19cc0a9b7aae8bc480aa22427bfd784b0a0d41846f67b93fc77432e3805245ce7e41e25a4988cae6d856ec16eebbbb3ebf56c44a85cbf600126120ea43a4c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ReSampleApp::Application.config.session_store :cookie_store, key: '_re_sample_app_session'
